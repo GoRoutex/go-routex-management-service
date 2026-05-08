1ed7ba82ec1a98a40dbab05ccb172b9dc43c90fe
Bao Pham <louis.phammm@gmail.com>
2026-05-08 20:01:10 +0700

    update: re-implement provinces and wards following vietnamese provinces

--- src/main/java/vn/com/routex/hub/management/service/infrastructure/persistence/jpa/provinces/entity/ProvincesEntity.java
+++ src/main/java/vn/com/routex/hub/management/service/infrastructure/persistence/jpa/provinces/entity/ProvincesEntity.java
@@ -3,8 +3,6 @@ package vn.com.routex.hub.management.service.infrastructure.persistence.jpa.prov
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
